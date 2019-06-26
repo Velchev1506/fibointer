0e8f2f5cfc0996b1d11469d9ede77a6dcb83a110
Velchev Vanes_begin_real <31736576+Velchev1506@users.noreply.github.com>
2019-06-26 14:32:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+a = input('a =')
+b = input('b =')
+
+
+class Fib:                                       
+    def __init__(self, max):                     
+        self.max = max
+
+    def __iter__(self):                          
+        self.a = 0
+        self.b = 1
+        return self
+
+    def __next__(self):                          
+        fib = self.a
+        if fib > self.max:
+            raise StopIteration                  
+        self.a, self.b = self.b, self.a + self.b
+        return fib               
+print(fib)
